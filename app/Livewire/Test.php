33d4dc54cc30864c5babb2e4fd73cd70c4a99589
pedro-dim <pcontato19@gmail.com>
2023-11-01 14:38:14 +0000

    first commit

---
+++ app/Livewire/Test.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Test extends Component
+{
+    public function render()
+    {
+        return view('livewire.test');
+    }
+
+
+    public function click(){
+dd('clicked');
+    }
+}
---
+++ resources/views/livewire/test.blade.php
@@ -0,0 +1,5 @@
+<div>
+
+    <h1>Livewire first component Test</h1>
+    <button class="bg-green-400 rounded p-1 text-white" wire:click="click">Click</button>
+</div>
